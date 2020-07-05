7830e6dde9d9f367205a3d765b7357f0eb66ca63
nkouki98 <farhan1998@live.com>
2020-07-05 23:46:44 +0600

    small update - order by date_posted for blog posts

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from .models import Post
 
 
 def home(request):
-    context = {'posts': Post.objects.all()}
+    context = {'posts': Post.objects.all().order_by('-date_posted')}
     return render(request, "blog/home.html", context)
--- users/models.py
+++ users/models.py
@@ -52,6 +52,7 @@ class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
+
     def __str__(self):
         return f'{self.user.username}'
 
