3149c1aea304714ee7a6e0615b2bba2cd1fd775d
nkouki98 <farhan1998@live.com>
2020-08-02 19:27:09 +0600

    minor fix

--- users/models.py
+++ users/models.py
@@ -59,7 +59,7 @@ class Profile(models.Model):
 
     #override save method
     def save(self, *args, **kwargs):
-        super().save()
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
