f7cb42d9fc6b399e0267cb466939bbb86eb226cd
Syed Farhan Ahmed <45247245+nkouki98@users.noreply.github.com>
2024-03-14 02:03:59 -0600

    package update

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -33,13 +33,15 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
